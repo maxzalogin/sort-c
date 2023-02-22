f8b12e18717d4e808cc24e3ff5b7fc15673bbf31
Максим Залогин <maxzalogin@MacBook-Air-Maksim.local>
2023-02-22 23:17:40 +0300

    dynamic memory

--- src/buble-sort.c
+++ src/buble-sort.c
@@ -1,56 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int generate(int* array, int size);
 void sort(int* array, int size);
 void print(int* array, int size);
 
 int main() {
-    int err = 0, size = 0;
+  int *p = NULL, value = 0, count = 0;
+  p = malloc(sizeof(int));
+  if (p != NULL) {
+    scanf("%d", &value);
+    do {
+      count++;
+      p = realloc(p, sizeof(int) * count);
+      p[count - 1] = value;
+      scanf("%d", &value);
+    } while (value != -1);
 
-    if ((scanf("%d", &size) != 1) || size <= 0) {
-        printf("n/a");
-    } else {
-        int array[size];
-        err = generate(array, size);
-        if (err == 1) {
-            printf("n/a");
-        } else {
-            sort(array, size);
-            print(array, size);
-        }
-    }
-    return 0;
+    sort(p, count);
+    print(p, count);
+    free(p);
+  } else
+    printf("Memory is not allocated");
+  return 0;
 }
 
-int generate(int* array, int size) {
-    int values = 0;
-    int flag = 0;
-    for (int i = 0; i < size; i++) {
-        if ((scanf("%d", &values) != 1)) {
-            flag++;
-            return flag;
-        }
-        array[i] = values;
+void sort(int* array, int len) {
+  for (int i = 0; i < len; i++) {
+    for (int j = 0; j < len - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        int tmp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = tmp;
+      }
     }
-    return flag;
+  }
 }
 
-void sort(int* array, int size) {
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                int tmp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = tmp;
-            }
-        }
-    }
-}
-
-void print(int* array, int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d", array[i]);
-        if (i != size - 1) printf(" ");
-    }
+void print(int* array, int len) {
+  for (int i = 0; i < len; i++) {
+    printf("%d", array[i]);
+    if (i != len - 1) 
+    printf(" ");
+  }
 }
