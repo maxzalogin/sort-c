70ecd9ba5c53e20f7e4324cf161a832e2ca0b2c3
Максим Залогин <maxzalogin@MacBook-Air-Maksim.local>
2023-02-23 11:01:18 +0300

    move function in sort.h

---
+++ src/sort.c
@@ -0,0 +1,11 @@
+void buble_sort(int* array, int len) {
+  for (int i = 0; i < len; i++) {
+    for (int j = 0; j < len - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        int tmp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = tmp;
+      }
+    }
+  }
+}
