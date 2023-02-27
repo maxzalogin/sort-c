3ed907694a81da30d4c77c71268e60934ff2afd0
Максим Залогин <maxzalogin@MacBook-Air-Maksim.local>
2023-02-27 17:23:27 +0300

    dynamic memory

--- src/main.c
+++ src/main.c
@@ -1,35 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "sort.h"
 
-void buble_sort(int* array, int size);
-void print(int* array, int size);
+void print(int* array, int len);
 
 int main() {
   int *p = NULL, value = 0, count = 0;
   p = malloc(sizeof(int));
-  if (p != NULL) {
+  scanf("%d", &value);
+  do {
+    count++;
+    p = realloc(p, sizeof(int) * count);
+    p[count - 1] = value;
     scanf("%d", &value);
-    do {
-      count++;
-      p = realloc(p, sizeof(int) * count);
-      p[count - 1] = value;
-      scanf("%d", &value);
-    } while (value != -1);
+  } 
+  
+  while (value != -1);
 
-    sort(p, count);
-    print(p, count);
-    free(p);
-  } else
-    printf("Memory is not allocated");
+  buble(p, count);
+  print(p, count);
+  free(p);
+
   return 0;
 }
-
 
 void print(int* array, int len) {
   for (int i = 0; i < len; i++) {
     printf("%d", array[i]);
-    if (i != len - 1) 
-    printf(" ");
+    if (i != len - 1) printf(" ");
   }
 }
