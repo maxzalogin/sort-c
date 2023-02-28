022d809b154301f3afc9e3450fb490aa90d289f2
max_zalogin <max.zalogin@gmail.com>
2023-02-28 18:43:20 +0300

    fix clang-format

--- src/main.c
+++ src/main.c
@@ -1,43 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h> 
+#include <time.h>
 
 #include "sort.h"
 
 void print(int* array, int len);
 
 int main() {
-  int *p = NULL, value = 0, count = 0;
-  p = malloc(sizeof(int));
-  scanf("%d", &value);
-  do {
-    count++;
-    p = realloc(p, sizeof(int) * count);
-    p[count - 1] = value;
+    int *p = NULL, value = 0, count = 0;
+    p = malloc(sizeof(int));
     scanf("%d", &value);
-  } 
-  
-  while (value != -1);
+    do {
+        count++;
+        p = realloc(p, sizeof(int) * count);
+        p[count - 1] = value;
+        scanf("%d", &value);
+    }
 
-  double time_spent = 0.0;
-  clock_t begin = clock();
+    while (value != -1);
 
-  // buble(p, count);
-  quick(p, 0, count - 1);
+    double time_spent = 0.0;
+    clock_t begin = clock();
 
-  clock_t end = clock();
-  time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-  print(p, count);
-  printf("\n\nTime: %f seconds", time_spent);
+    // buble(p, count);
+    quick(p, 0, count - 1);
 
-  free(p);
+    clock_t end = clock();
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+    print(p, count);
+    printf("\n\nTime: %f seconds", time_spent);
 
-  return 0;
+    free(p);
+
+    return 0;
 }
 
 void print(int* array, int len) {
-  for (int i = 0; i < len; i++) {
-    printf("%d", array[i]);
-    if (i != len - 1) printf(" ");
-  }
+    for (int i = 0; i < len; i++) {
+        printf("%d", array[i]);
+        if (i != len - 1) printf(" ");
+    }
 }
--- src/sort.c
+++ src/sort.c
@@ -1,35 +1,33 @@
 void buble(int* array, int len) {
-  for (int i = 0; i < len; i++) {
-    for (int j = 0; j < len - i - 1; j++) {
-      if (array[j] > array[j + 1]) {
-        int tmp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = tmp;
-      }
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < len - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                int tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
     }
-  }
 }
 
 void quick(int* array, int first, int last) {
-if (first < last) {
-int left = first;
-int right = last;
-int center = array[(first + last) / 2];
-do {
-while (array[left] < center) 
-left++;
-while (array[right] > center) 
-right--;
+    if (first < last) {
+        int left = first;
+        int right = last;
+        int center = array[(first + last) / 2];
+        do {
+            while (array[left] < center) left++;
+            while (array[right] > center) right--;
 
-if (left <= right) {
-int tmp = array[left];
-array[left] = array[right];
-array[right] = tmp;
-left++;
-right--;
-}
-} while (left < right);
-  quick(array, first, right);
-  quick(array, left, last);
-}
+            if (left <= right) {
+                int tmp = array[left];
+                array[left] = array[right];
+                array[right] = tmp;
+                left++;
+                right--;
+            }
+        } while (left < right);
+        quick(array, first, right);
+        quick(array, left, last);
+    }
 }
